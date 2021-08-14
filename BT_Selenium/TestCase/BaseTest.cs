2c788d1b016991b78bc7d4462e099e3c1f5561da
Nestor Flores <nestorchoele@gmail.com>
2021-08-14 19:28:26 -0300

    first commit nueva rama - Refactoring a Screenplay desde casa

--- BT_Selenium/TestCase/BaseTest.cs
+++ BT_Selenium/TestCase/BaseTest.cs
@@ -3,10 +3,6 @@ using BT_Selenium.PageObject;
 using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BT_Selenium.Handler;
 using BT_Selenium.PageObject.WebPanel;
 
@@ -15,12 +11,15 @@ namespace BT_Selenium.TestCase
     /*
      * Clase base para todos los test
      */
-    public abstract class BaseTest
+    public class BaseTest
     {
         //Selenium Driver
         protected IWebDriver driver;
+        protected string QaURL = "http://btwebqa.ar.bpn/BTWeb/hlogin.aspx";
+        protected string DfURL = "";
+        protected string ProdURL = "";
 
-               
+
         [SetUp]
         public void BeforeBaseTest()
         {
@@ -28,7 +27,7 @@ namespace BT_Selenium.TestCase
             options.EnsureCleanSession = true;
             options.RequireWindowFocus = true;
             driver = new InternetExplorerDriver("C:\\webdriver\\");
-            driver.Navigate().GoToUrl("http://btwebqa.ar.bpn/BTWeb/hlogin.aspx");
+            driver.Navigate().GoToUrl(QaURL);
             driver.Manage().Window.Maximize();
             LoginPage loginPage = new LoginPage(driver);
             loginPage.Ingresar();
