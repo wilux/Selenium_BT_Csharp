fcf26afc2d1646a94be34d71e700f72fb4118c89
Nestor Flores <nestorchoele@gmail.com>
2021-08-18 14:09:57 -0300

    Credenciales

---
+++ BT_Selenium/CredencialesTamplate.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_Selenium
+{
+    //Eliminar "Tamplate" del nombre del archivo
+    //Tanto el archivo como la clase debe llamarse "Credenciales"
+    //Esto es un template
+    public class CredencialesTamplate
+    {
+        public string usuario = "usuario";
+        public string password = "password";
+    }
+}
