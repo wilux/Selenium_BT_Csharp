b47c02bd09405ba9c143be816b2d9264ac7abe42
Nestor Flores <nestorchoele@gmail.com>
2021-08-18 13:29:53 -0300

    Credenciales

---
+++ BT_Selenium/PageObject/Credenciales.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_Selenium.PageObject
+{
+    class Credenciales
+    {
+        public string usuario = "floresnes";
+        public string password = "Carme2022";
+    }
+}
